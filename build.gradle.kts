17ce74d7269c38d4a1a3ea01937360120c1f3453
hamid-darzi-dev <158063826+hamid-darzi-dev@users.noreply.github.com>
2025-06-07 22:23:03 +0200

    DIVARAK-0002 Add Initial Spring Boot Template  (#2)
    * Divarak-0002 add .git and .idea
    
    * Divarak-0002 add git files
    
    * Divarak-0002 add gradle files
    
    * Divarak-0002 Add DivarakApplication and test classes
    
    * Divarak-0002 Add application.yml file
    
    * Divarak-0002 Modifiy .idea files
    
    * Divarak-0002 Add version catalog
    
    * Divarak-0002 Update Build_gradle
    
    * Divarak-0002 Add datasource to application-local.yml
    
    * Divarak-0002 Add docker-compose.yaml to add postgres database container

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+	alias(libs.plugins.kotlin.jvm)
+	alias(libs.plugins.kotlin.spring)
+	alias(libs.plugins.spring.boot)
+	alias(libs.plugins.spring.dependency.management)
+}
+
+group = "com.divarak"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation(libs.spring.boot.starter.jooq)
+	implementation(libs.spring.boot.starter.web)
+	implementation(libs.jackson.module.kotlin)
+	implementation(libs.flyway.core)
+	implementation(libs.flyway.postgres)
+	implementation(libs.kotlin.reflect)
+	implementation(libs.postgresql)
+
+	testImplementation(libs.spring.boot.starter.test)
+	testImplementation(libs.kotlin.test.junit5)
+	testRuntimeOnly(libs.junit.platform.launcher)
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/kotlin/com/divarak/DivarakApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.divarak
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DivarakApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
