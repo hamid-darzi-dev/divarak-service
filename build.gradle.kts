a647bdd6ccbf7d5708c9cfb139678327611b3102
hamid-darzi-dev <158063826+hamid-darzi-dev@users.noreply.github.com>
2025-06-08 13:32:59 +0200

    DIVARAK-0003 Implement `spotless` Linter (#3)
    * DIVARAK-0003 Add spotless libraries
    
    * DIVARAK-0003 Add ktlint dependency and spotless plugin
    
    * DIVARAK-0003 Add .editorconfig
    
    * DIVARAK-0003 Apply spotless
    
    * DIVARAK-0003 Make KotlinCompile depend on spotlessApply

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,43 +1,98 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-	alias(libs.plugins.kotlin.jvm)
-	alias(libs.plugins.kotlin.spring)
-	alias(libs.plugins.spring.boot)
-	alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.spotless)
 }
 
 group = "com.divarak"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation(libs.spring.boot.starter.jooq)
-	implementation(libs.spring.boot.starter.web)
-	implementation(libs.jackson.module.kotlin)
-	implementation(libs.flyway.core)
-	implementation(libs.flyway.postgres)
-	implementation(libs.kotlin.reflect)
-	implementation(libs.postgresql)
+    implementation(libs.spring.boot.starter.jooq)
+    implementation(libs.spring.boot.starter.web)
+    implementation(libs.jackson.module.kotlin)
+    implementation(libs.flyway.core)
+    implementation(libs.flyway.postgres)
+    implementation(libs.kotlin.reflect)
+    implementation(libs.postgresql)
 
-	testImplementation(libs.spring.boot.starter.test)
-	testImplementation(libs.kotlin.test.junit5)
-	testRuntimeOnly(libs.junit.platform.launcher)
+    implementation(libs.pinterest.ktlint)
+
+    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.kotlin.test.junit5)
+    testRuntimeOnly(libs.junit.platform.launcher)
 }
 
 kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
+}
+
+spotless {
+    val ktlintVersion =
+        libs.versions.pinterest.ktlint
+            .get()
+
+    val targetExclusions =
+        arrayOf(
+            "build/**/*",
+            ".gradle/**/*",
+            ".idea/**/*",
+        )
+
+    isEnforceCheck = true
+
+    kotlin {
+        ktlint(ktlintVersion)
+        targetExclude(targetExclusions)
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+
+    kotlinGradle {
+        ktlint(ktlintVersion)
+        targetExclude(targetExclusions)
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+
+    format("miscellaneous") {
+        target(
+            "**/.json",
+            "**.*.graphql*",
+            "**/.gitignore",
+            "**/.gitignore",
+            "**/*.properties",
+            "**/*.md",
+            "**/*.xml",
+            "**/*.yaml",
+            "**/*.yml",
+        )
+        targetExclude(*targetExclusions)
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+
+    tasks.withType<KotlinCompile> {
+        dependsOn(tasks.spotlessApply)
+    }
 }
