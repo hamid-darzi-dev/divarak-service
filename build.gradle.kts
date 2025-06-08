51d628b8fe92b49e3fab4e9bde354b5c1b8ece9d
hamiddarzi <hamid.darzi.dev@gmail.com>
2025-06-08 12:36:37 +0200

    DIVARAK-0003 Apply spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,90 +1,92 @@
 plugins {
-	alias(libs.plugins.kotlin.jvm)
-	alias(libs.plugins.kotlin.spring)
-	alias(libs.plugins.spring.boot)
-	alias(libs.plugins.spring.dependency.management)
-	alias(libs.plugins.spotless)
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.spotless)
 }
 
 group = "com.divarak"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation(libs.spring.boot.starter.jooq)
-	implementation(libs.spring.boot.starter.web)
-	implementation(libs.jackson.module.kotlin)
-	implementation(libs.flyway.core)
-	implementation(libs.flyway.postgres)
-	implementation(libs.kotlin.reflect)
-	implementation(libs.postgresql)
+    implementation(libs.spring.boot.starter.jooq)
+    implementation(libs.spring.boot.starter.web)
+    implementation(libs.jackson.module.kotlin)
+    implementation(libs.flyway.core)
+    implementation(libs.flyway.postgres)
+    implementation(libs.kotlin.reflect)
+    implementation(libs.postgresql)
 
-	implementation(libs.pinterest.ktlint)
+    implementation(libs.pinterest.ktlint)
 
-	testImplementation(libs.spring.boot.starter.test)
-	testImplementation(libs.kotlin.test.junit5)
-	testRuntimeOnly(libs.junit.platform.launcher)
+    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.kotlin.test.junit5)
+    testRuntimeOnly(libs.junit.platform.launcher)
 }
 
 kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 spotless {
-	val ktlintVersion = libs.versions.pinterest.ktlint.get()
+    val ktlintVersion =
+        libs.versions.pinterest.ktlint
+            .get()
 
-	val targetExclusions =
-		arrayOf(
-			"build/**/*",
-			".gradle/**/*",
-			".idea/**/*",
-		)
+    val targetExclusions =
+        arrayOf(
+            "build/**/*",
+            ".gradle/**/*",
+            ".idea/**/*",
+        )
 
-	isEnforceCheck = true
+    isEnforceCheck = true
 
-	kotlin {
-		ktlint(ktlintVersion)
-		targetExclude(targetExclusions)
-		trimTrailingWhitespace()
-		endWithNewline()
-	}
+    kotlin {
+        ktlint(ktlintVersion)
+        targetExclude(targetExclusions)
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
 
-	kotlinGradle {
-		ktlint(ktlintVersion)
-		targetExclude(targetExclusions)
-		trimTrailingWhitespace()
-		endWithNewline()
-	}
+    kotlinGradle {
+        ktlint(ktlintVersion)
+        targetExclude(targetExclusions)
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
 
-	format("miscellaneous") {
-		target(
-			"**/.json",
-			"**.*.graphql*",
-			"**/.gitignore",
-			"**/.gitignore",
-			"**/*.properties",
-			"**/*.md",
-			"**/*.xml",
-			"**/*.yaml",
-			"**/*.yml",
-		)
-		targetExclude(*targetExclusions)
-		trimTrailingWhitespace()
-		endWithNewline()
-	}
+    format("miscellaneous") {
+        target(
+            "**/.json",
+            "**.*.graphql*",
+            "**/.gitignore",
+            "**/.gitignore",
+            "**/*.properties",
+            "**/*.md",
+            "**/*.xml",
+            "**/*.yaml",
+            "**/*.yml",
+        )
+        targetExclude(*targetExclusions)
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
 }
