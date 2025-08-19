83e7b6775229cc3dac151d3581aa053f0d000c3b
hamiddarzi <hamid.darzi.dev@gmail.com>
2025-08-19 22:00:20 +0200

    DIVARAK-0007 Add test controller

---
+++ src/main/kotlin/com/divarak/interfacee/web/ListingController.kt
@@ -0,0 +1,32 @@
+package com.divarak.interfacee.web
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/listings")
+class ListingController {
+    private val listings =
+        listOf(
+            Listing(1, "Car"),
+            Listing(2, "Apartment"),
+            Listing(3, "TV"),
+        )
+
+    private val listingsMap = listings.associateBy { it.id }
+
+    @GetMapping("/{id}")
+    fun getListing(
+        @PathVariable id: Int,
+    ): ResponseEntity<Listing> {
+        val listing = listingsMap[id]
+        return if (listing != null) {
+            ResponseEntity.ok(listing)
+        } else {
+            ResponseEntity.notFound().build()
+        }
+    }
+}
