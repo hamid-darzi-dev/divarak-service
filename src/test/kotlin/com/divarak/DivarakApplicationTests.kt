7347a666cfa88b0a347888b1ff9f57df68143a80
hamiddarzi <hamid.darzi.dev@gmail.com>
2025-08-31 12:09:40 +0200

    DIVARAK-0012 Add testcontainer to integration test

--- src/test/kotlin/com/divarak/DivarakApplicationTests.kt
+++ src/test/kotlin/com/divarak/DivarakApplicationTests.kt
@@ -2,9 +2,33 @@ package com.divarak
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 
 @SpringBootTest
+@Testcontainers
 class DivarakApplicationTests {
+    companion object {
+        @Container
+        val postgres =
+            PostgreSQLContainer<Nothing>("postgres:16").apply {
+                withDatabaseName("testdb")
+                withUsername("user")
+                withPassword("password")
+            }
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun properties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", postgres::getJdbcUrl)
+            registry.add("spring.datasource.username", postgres::getUsername)
+            registry.add("spring.datasource.password", postgres::getPassword)
+        }
+    }
+
     @Test
     fun contextLoads() {
     }
